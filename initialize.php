819eefe890c8a07df8ea09491e16af2dbf58af2a
sn0wmanmj <sn0wmanmj@users.noreply.github.com>
2019-01-14 21:40:16 +0100

    changed length of token in db

--- initialize.php
+++ initialize.php
@@ -20,7 +20,7 @@ $sql_macAddressTable = "CREATE TABLE IF NOT EXISTS macs (
     mac VARCHAR(17) NOT NULL,
     deviceName VARCHAR(60),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    token VARCHAR(30),
+    token VARCHAR(60),
     verified BOOL DEFAULT FALSE,
     FOREIGN KEY (userId) REFERENCES users(id)
 )";
