abc1236a34c648e36974b068b0df31ab79ad45e6
sn0wmanmj <sn0wmanmj@users.noreply.github.com>
2019-01-27 18:40:37 +0100

    fixed typo

--- delete.php
+++ delete.php
@@ -7,7 +7,7 @@ $urlToken = $_GET["token"];
 $deleteMac = $db_conn->prepare("DELETE FROM macs WHERE (token = ?)");
 $deleteMac->bind_param("s", $urlToken);
 if ($deleteMac->execute()) {
-    echo "Geräte-Adresse erfolgreich registriert.";
+    echo "Geräte-Adresse erfolgreich gelöscht.";
 } else {
     echo "Etwas ist schiefgegangen";
 }
