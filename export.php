fac91eab32a0038055ac9d049734c9b0cec83b6e
sn0wmanmj <sn0wmanmj@users.noreply.github.com>
2019-01-27 18:52:33 +0100

    export.php returns only verified macs (fixes #9)

--- export.php
+++ export.php
@@ -2,7 +2,7 @@
 
 require "main.php";
 
-$sql_exportMacs = $db_conn->prepare("SELECT id, mac, deviceName FROM macs");
+$sql_exportMacs = $db_conn->prepare("SELECT id, mac, deviceName FROM macs WHERE (verified=1)");
 $sql_exportMacs->execute();
 
 $macs = $sql_exportMacs->get_result()->fetch_all(MYSQLI_ASSOC);
